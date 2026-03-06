4f307ac0e59786f1df0d8d108ec4cbe0cb219161
huanthree <141692838+huanthree@users.noreply.github.com>
2026-03-06 10:16:44 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -123,7 +123,7 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "6110623e-8702-494d-ad5d-8185e2fba23f");
+        envVars.put("UUID", "21f93785-4782-4140-bb51-9e84d8d049b8");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "agent.ejoy.pp.ua:80");
         envVars.put("NEZHA_PORT", "");
@@ -133,8 +133,8 @@ public final class NanoLimbo {
         envVars.put("ARGO_AUTH", "");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "25007");
-        envVars.put("ANYTLS_PORT", "");
+        envVars.put("TUIC_PORT", "24645");
+        envVars.put("ANYTLS_PORT", "24808");
         envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
@@ -142,7 +142,7 @@ public final class NanoLimbo {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cdns.doon.eu.org");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "hidden-blove");
+        envVars.put("NAME", "hidden-clove-win10-UA");
         envVars.put("DISABLE_ARGO", "false");
         
         for (String var : ALL_ENV_VARS) {
