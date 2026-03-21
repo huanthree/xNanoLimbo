744c944e792e480e100f1375222d9ce2475b7cec
huanthree <141692838+huanthree@users.noreply.github.com>
2026-03-21 12:26:00 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -132,10 +132,10 @@ public final class NanoLimbo {
         envVars.put("ARGO_DOMAIN", "minerackargo.foollove.qzz.io");
         envVars.put("ARGO_AUTH", "eyJhIjoiODljOTg0OWQyODlkYzNiMDRiOTFjNDViM2RiZTlhOGQiLCJ0IjoiMWI4Mjk3NWItNDQ2MC00ZTUxLWEwNmYtNjIzNjA2NGZhOTczIiwicyI6IllqZ3pOR016TVRndE9USTJOeTAwTmpsa0xXRXdPVFl0T1RkaU9XWTNPVFpqWTJNNCJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "25619");
+        envVars.put("HY2_PORT", "25629");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "25619");
+        envVars.put("REALITY_PORT", "25629");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
